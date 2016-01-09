82082c0c97c11fb4226df58b4a53799641c7c035
Nick Marucci <npm1514@gmail.com>
2016-01-09 15:02:42 -0700

    commit star wars app

---
+++ mainCtrl.js
@@ -0,0 +1,8 @@
+angular.module("app").controller("mainCtrl", function($scope, mainService) {
+  $scope.getPlanets = function() {
+    mainService.getPlanets().then(function(response){
+      $scope.planets = response;
+    });
+  };
+  $scope.getPlanets();
+});
---
+++ mainService.js
@@ -0,0 +1,22 @@
+angular.module("app").service("mainService", function($http, $q) {
+  var baseURL = "http://swapi.co/api/";
+
+  this.getPlanets = function() {
+  //   var deferrer = $q.defer();
+  //   $timeout(function() {
+  //     if (response) {
+  //       deferrer.resolve("Sparkly Unicorn");
+  //     } else {
+  //       deferrer.reject("Unicorns do not exist, :scrunchyface:");
+  //     }
+  //   }, 2000);
+  //   return deferrer.promise;
+  // };
+    return $http({
+      method: "GET",
+      url: baseURL + "planets/"
+    }).then (function(response){
+      return(response.data.results);
+    });
+  };
+});
